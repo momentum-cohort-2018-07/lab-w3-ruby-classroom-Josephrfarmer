7ec53c20356725ab437fa73f560ecdb808a9eb8e
josephrfarmer <josephr.farmer24@gmail.com>
2018-08-13 18:39:04 -0400

    first commit of homework
    completed.

--- starter_files/classroom.rb
+++ starter_files/classroom.rb
@@ -2,21 +2,36 @@
 # for that student and assignment. Note that Ruby counts arrays from 0, but
 # we are referring to them as 1-10.
 
-
+def assignment_score(data, name, assignment)
+        data[name] [assignment-1]
+end
 # Given a grade_hash and assignment number, return all scores for that
 # assignment. Note that Ruby counts arrays from 0, but we are referring to
 # them as 1-10.
+def assignment_scores(data, assignment)
+    data.values.map do |scores| 
+        scores[assignment -1]
+    end
+end
 
 
 # Given a grade_hash and assignment number, return the average score for that
 # assignment. Note that Ruby counts arrays from 0, but we are referring to
 # them as 1-10.
+def assignment_average_score(data, assignment)
+    scores = assignment_scores(data, assignment)
+    scores.sum/scores.length
+end
 
 
 # Return a hash of students and their average score.
 # TIP: To convert an array like [[:indiana, 90], [:nevada, 80]] to a hash,
 # use .to_h. Also look at Hash#transform_values.
-
+def averages(data)
+    data.transform_values do |scores|
+        scores.sum/scores.length
+    end
+end
 
 # Return a letter grade for a numerical score.
 # 90+ => A
@@ -24,13 +39,36 @@
 # 70-79 => C
 # 60-69 => D
 # < 60 => F
+def letter_grade(score)
+    if score >= 90
+        "A"
+    elsif score >= 80
+        "B"
+    elsif score >= 70
+        "C"
+    elsif score >= 60
+        "D"
+    else
+        "F"
+    end
+end
 
 # Return a hash of students and their final letter grade, as determined
 # by their average.
+def final_letter_grades(data)
+    averages(data).transform_values do |scores|
+        letter_grade(scores)
+    end
+end
 
 
 # Return the average for the entire class.
 
+def class_average(data)
+    scores = averages(data).values
+    scores.sum/scores.length
+end
 
 # Return an array of the top `number_of_students` students.
+
 
